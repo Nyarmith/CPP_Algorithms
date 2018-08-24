67ea0400e8ba5adf7ab44ec6c816d7628f37a1d1
nyarmith <powah.serge@gmail.com>
2018-08-23 22:22:22 -0500

    Implemented cool rational number class

--- Problems/a2oj_ladders/lt1400/80_codeforces_337B.cpp
+++ Problems/a2oj_ladders/lt1400/80_codeforces_337B.cpp
@@ -12,46 +12,120 @@
 #include <queue>
 #define ll long long
 
+
+ll gcd(ll a, ll b){
+  if (a > b)
+    std::swap(a,b);
+
+  if (b%a != 0){
+    return gcd(a,b%a);
+  } else {
+    return a;
+  }
+}
+
+ll lcm(ll a, ll b){
+  return std::abs(a*b)/gcd(a,b);
+}
+
 struct Ratio{
   ll num, den; //numerator, denominator, respectively
-  Ratio(ll num, ll den) : num(num), den(den)
+
+  Ratio(ll n) : num(n), den(1) {}
+  Ratio(ll n, ll d) : num(n), den(d)
   {
-    //use gcd to simplify num and denom
+    ll m = gcd(num,den);
+    num /= m;
+    den /= m;
   }
 
-  bool operator==(const Ratio& o)
+  bool operator==(const Ratio& o) const
   {
     return num == o.num && den == o.den;
   }
 
-  Ratio operator-(const Ratio& o)
+  Ratio operator-(const Ratio& o) const
   {
-    ll t_num, t_denom;
-    if (den != o.den){
-      //do some gcd
-    }
-    return Ratio{t_num, t_denom};
+    //do some lcm
+    ll l = lcm(den, o.den);
+    ll our_num = l*num/den;
+    ll their_num = l*o.num/o.den;
+
+    return {our_num - their_num, l};
   }
 
-  Ratio operator+(const Ratio& o)
+  Ratio operator*(const Ratio& o) const{
+    return {num*o.num, den*o.den};
+  }
+
+  Ratio operator/(const Ratio& o) const{
+    return {num*o.den, den*o.num};
+  }
+
+  Ratio operator+(const Ratio& o) const
   {
-    ll t_num, t_denom;
-    if (den != o.den){
-      //do some gcd
-    }
-    return Ratio{t_num, t_denom};
+    ll l = lcm(den, o.den);
+    ll our_num = l*num/den;
+    ll their_num = l*o.num/o.den;
+
+    return {our_num + their_num, l};
+  }
+
+  bool operator<(const Ratio& o) const {
+    //common denominator and all that
+    ll common = lcm(den, o.den);
+    ll our_num = common*num/den;
+    ll their_num = common*o.num/o.den;
+
+    return (our_num < their_num);
+  }
+
+  bool operator<=(const Ratio& o) const {
+    return (*this < o || *this == o);
   }
 };
 
 //I haven't actually interfaced the problem though; that's the real challenge
 
+
 int main(){
   ll a,b, // ratio 1
      c,d; // ratio 2
+
   //goal: calculate ratio of empty screen to the total screen size
   std::cin >> a >> b
            >> c >> d;
 
+  // step 1, simplify with gcd
+  ll m = gcd(a,b);
+  a /= m;
+  b /= m;
 
+  m = gcd(c,d);
+  c /= m;
+  d /= m;
+
+  //recall: lcm = (a*b)/gcd(a,b)
+
+
+  Ratio ret{1};
+
+  //we can break this down into 4 trivial cases
+  if ( a <= c && b <= d) {
+    //both must be scaled up by either c/a or d/b
+    Ratio r1{c,a}, r2{d,b};
+    if (r1*b <= d){
+    }
+
+  } else if (a <= c && b > d) {
+    //scale both down by d/b
+
+  } else if (a > c && b <= d) {
+    //scale both down by c/a
+
+  } else { //a>c && b>d
+    //both must be scaled down by either c/a or d/b
+
+  }
 }
 
